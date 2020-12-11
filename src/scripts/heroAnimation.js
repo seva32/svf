a034fa9d27607ce954e0cf5b2614bd3667b1b010
seva32 <seva32@yahoo.es>
2020-12-11 20:19:30 -0300

    anim

--- src/scripts/heroAnimation.js
+++ src/scripts/heroAnimation.js
@@ -16,7 +16,109 @@ var callback = function (entries) {
     //   entry.target,
     //   entry.time,
     if (entry.intersectionRatio) {
-      console.log('entro');
+      const words = document.querySelectorAll('.hero-img svg g');
+      const colors = [
+        '#5c27c1ff',
+        '#9dacffff',
+        '#8093F1',
+        '#d81159ff',
+        '#72DDF7',
+        '#F7AEF8',
+      ];
+
+      const heroAnimation = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 6000,
+      });
+
+      heroAnimation
+        .add({
+          targets: '.hero-img',
+          opacity: [0, 1],
+          begin(anim) {
+            let i;
+            for (i = 0; i < words.length; ++i) {
+              if (i === 1) {
+                const tl = anime.timeline({
+                  duration: 2000,
+                  delay: 2700,
+                });
+                tl.add({
+                  targets: words[1],
+                  opacity: [0, 1],
+                  scale: [0.2, 1],
+                  translateX: '0%',
+                  translateY: '0%',
+                });
+              } else if (i < 5) {
+                const tl = anime.timeline({
+                  duration: 2500,
+                  delay: 500,
+                });
+                tl.add({
+                  targets: words[i],
+                  keyframes: [
+                    { translateY: 40 },
+                    { translateX: -50 },
+                    { translateY: -40 },
+                    { translateX: 0 },
+                    { translateY: 0 },
+                  ],
+                  easing: 'easeOutElastic(1, .8)',
+                });
+              } else if (i < 10) {
+                const tl = anime.timeline({
+                  duration: 1500,
+                });
+                tl.add({
+                  targets: words[i],
+                  easing: 'easeOutBounce',
+                  translateX: ['-1000px', '0px'],
+                  opacity: [0, 1],
+                });
+              } else {
+                const tl = anime.timeline({
+                  duration: 3000,
+                });
+                tl.add({
+                  targets: words[i],
+                  keyframes: [
+                    { translateY: -20 },
+                    { translateX: 200 },
+                    { translateY: 20 },
+                    { translateX: 0 },
+                    { translateY: 0 },
+                  ],
+                  easing: 'easeOutElastic(1, .8)',
+                });
+              }
+            }
+          },
+        })
+        .add(
+          {
+            targets: words,
+            duration: 1000,
+            strokeWidth: ['3px', '1px'],
+            stroke(el, i, l) {
+              return colors[anime.random(0, 5)];
+            },
+            loop: 3,
+            easing: 'easeInOutBounce',
+            delay(el, i, l) {
+              return i * 100;
+            },
+            endDelay(el, i, l) {
+              return (l - i) * 100;
+            },
+          },
+          '+=3000',
+        )
+        .add({
+          targets: words,
+          stroke: '#d81159ff',
+          duration: 300,
+        });
     }
   });
 };
