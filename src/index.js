545a48efa72b37888c25d0894168a96d3d15ccfd
Sebas <sebas.warsaw@gmail.com>
2020-12-24 22:23:36 -0300

    svgs words ok

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import loadImgSrc from './scripts/loadImgSrc';
 import preloadAnimation from './scripts/preloadAnimation';
 import contactBtnAnimation from './scripts/contactBtnAnimation';
 import loadHTML from './scripts/loadHTML';
-import sideBarAnimation from './scripts/sideBarAnimation';
-import heroAnimation from './scripts/heroAnimation';
+// import sideBarAnimation from './scripts/sideBarAnimation';
+// import heroAnimation from './scripts/heroAnimation';
 
 import './styles/style.css';
 
