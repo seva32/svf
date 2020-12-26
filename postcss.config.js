294da0cc2dd4551edf159c2b62d41db3157b5d0f
Sebas <sebas.warsaw@gmail.com>
2020-12-26 16:24:09 -0300

    postcss fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssFocusWithin = require('postcss-focus-within');
+
 module.exports = {
   plugins: [
-    require('postcss-preset-env')(),
-    require('cssnano')(),
+    require('cssnano'),
+    require('autoprefixer'),
+    postcssFocusWithin({ replaceWith: '.focus-within' }),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-import'),
+    require('postcss-for'),
+    require('postcss-mixins'),
+    require('postcss-calc'),
+    postcssPresetEnv({
+      stage: 0,
+      features: {
+        'nesting-rules': true,
+        'color-mod-function': true,
+        'custom-media': true,
+        'focus-within-pseudo-class': false,
+      },
+    }),
   ],
 };
