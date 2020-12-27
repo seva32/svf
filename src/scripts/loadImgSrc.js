2203106552b44796d26178cc956b817d6cd1fdca
Sebas <sebas.warsaw@gmail.com>
2020-12-27 19:17:40 -0300

    figma ship

--- src/scripts/loadImgSrc.js
+++ src/scripts/loadImgSrc.js
@@ -1,4 +1,4 @@
 export default () => {
   document.getElementById('plane').src =
-    'https://res.cloudinary.com/seva32/image/upload/v1607995881/jetIcon_iutoyi.jpg';
+    'https://res.cloudinary.com/seva32/image/upload/v1609107380/figmaship_ygseqi.png';
 };
