5a3bc8b59256a12eb9b491288b7d44238804b30d
Sebas <sebas.warsaw@gmail.com>
2021-06-18 22:35:12 -0300

    fix plane

--- src/scripts/loadImgSrc.js
+++ src/scripts/loadImgSrc.js
@@ -1,4 +1,4 @@
 export default () => {
   document.getElementById('plane').src =
-    'https://res.cloudinary.com/seva32/image/upload/v1609107380/figmaship_ygseqi.png';
+    'https://res.cloudinary.com/seva32/image/upload/v1624066392/newPlane_pyaene.png';
 };
