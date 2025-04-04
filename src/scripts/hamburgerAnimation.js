f07f7c6e04dde53a99e7927961151a4be0f3c18c
seva32 <seva32@yahoo.es>
2025-04-04 11:49:54 -0400

    restyle menu

--- src/scripts/hamburgerAnimation.js
+++ src/scripts/hamburgerAnimation.js
@@ -1,110 +1,97 @@
+/* eslint-disable indent */
 import { vw } from './utils';
 
-const backdropScale = vw > 550 ? 45 : 25;
-const itemsTranslateX = vw > 550 ? '2rem' : '1.5rem';
-const itemsScale = vw > 550 ? 7 : 5;
+const itemsTranslateX = vw > 550;
 
-const hamWrapper = document.querySelector('.hamburger-wrapper');
-const wavesWrapper = document.querySelector('.waves-container');
-
-const backdropTL = anime.timeline({ autoplay: false, loop: 1 });
-backdropTL.add({
-  targets: '.hamburger-backdrop',
-  zIndex: [{ value: [-2, 5], duration: 1 }],
-  opacity: [{ value: [0, 1], duration: 1 }],
-  scale: [{ value: [1, backdropScale], duration: 500 }],
+const sidebarTL = anime.timeline({ autoplay: false, loop: 1 });
+sidebarTL.add({
+  targets: '.side-wrapper',
+  opacity: [
+    { value: 1, delay: 200 },
+    { value: 0, duration: 1500 },
+    { value: 1, duration: 500 },
+  ],
 });
-backdropTL.pause();
 
-const itemsArr = document.querySelectorAll('.hamburger-items div');
+const animationProps = {
+  begin() {
+    sidebarTL.play();
+  },
+  scale: [
+    { value: [1, 1.2], duration: 800, easing: 'easeOutQuad', delay: 1000 },
+  ],
+  translateX: itemsTranslateX
+    ? [
+        { value: '-3px', duration: 500, delay: 500 },
+        { value: '10px', duration: 1400 },
+      ]
+    : undefined,
+  translateY: !itemsTranslateX
+    ? [
+        { value: '-3px', duration: 500, delay: 500 },
+        { value: '10px', duration: 1400 },
+      ]
+    : undefined,
+};
+const projectsTL = anime.timeline({ autoplay: false, loop: 1 });
+projectsTL.add({
+  targets: '.hamburger-items button#projects-link',
+  ...animationProps,
+});
+const skillTL = anime.timeline({ autoplay: false, loop: 1 });
+skillTL.add({
+  targets: '.hamburger-items button#skills-link',
+  ...animationProps,
+});
+const homeTL = anime.timeline({ autoplay: false, loop: 1 });
+homeTL.add({
+  targets: '.hamburger-items button#home-link',
+  ...animationProps,
+});
+const toggleHamburgerItem = (e) => {
+  const item = e.currentTarget;
+  const itemIndex = item.getAttribute('data-index');
 
-const toggleHamburger = () => {
-  hamWrapper.classList.toggle('hamburger-opened');
-  hamWrapper.setAttribute(
-    'aria-expanded',
-    hamWrapper.classList.contains('hamburger-opened'),
-  );
-
-  if (hamWrapper.classList.contains('hamburger-opened')) {
-    // backdropTL.seek(0);
-    if (backdropTL.reversed) {
-      backdropTL.reverse();
-    }
-    backdropTL.play();
-
-    document.querySelector('.hamburger-items').style.zIndex = '10';
-    document.querySelector('.contact-btn-container').style.opacity = '0';
-
-    anime({
-      targets: itemsArr,
-      opacity: [{ value: [0, 1], duration: 1 }],
-      scale: [{ value: [1, itemsScale], duration: 800 }],
-      translateY(el, i, l) {
-        return `${1 * (i + 1)}rem`;
-      },
-      translateX: itemsTranslateX,
-      complete(anim) {
-        wavesWrapper.style.display = 'block';
-        anime({
-          targets: wavesWrapper,
-          opacity: [0, 1],
-          duration: 3500,
-        });
-      },
-    });
+  if (projectsTL.progress === 100) {
+    projectsTL.reverse();
+    projectsTL.play();
   }
-
-  if (!hamWrapper.classList.contains('hamburger-opened')) {
-    backdropTL.reverse();
-    backdropTL.play();
-
-    document.querySelector('.hamburger-items').style.zIndex = '-1';
-
-    anime({
-      targets: itemsArr,
-      opacity: [{ value: [1, 0], duration: 1 }],
-      scale: [{ value: [10, 1], duration: 500 }],
-      translateY: '0px',
-      translateX: '0rem',
-      begin(an) {
-        wavesWrapper.style.display = 'none';
-        anime({
-          targets: wavesWrapper,
-          opacity: [1, 0],
-          duration: 200,
-        });
-      },
-      complete(anim) {
-        document.querySelector('.contact-btn-container').style.opacity = '1';
-      },
-    });
+  if (skillTL.progress === 100) {
+    skillTL.reverse();
+    skillTL.play();
+  }
+  if (homeTL.progress === 100) {
+    homeTL.reverse();
+    homeTL.play();
+  }
+  if (itemIndex === 'projects') {
+    if (projectsTL.reversed) {
+      projectsTL.reverse();
+    }
+    projectsTL.restart();
+  } else if (itemIndex === 'skills') {
+    if (skillTL.reversed) {
+      skillTL.reverse();
+    }
+    skillTL.restart();
+  } else if (itemIndex === 'home') {
+    if (homeTL.reversed) {
+      homeTL.reverse();
+    }
+    homeTL.restart();
   }
 };
-
-if (hamWrapper) {
-  hamWrapper.addEventListener('click', toggleHamburger);
-}
 
 const aArr = document.querySelectorAll('.hamburger-items div button');
 
 for (let s = 0; s < aArr.length; s++) {
   aArr[s].addEventListener(
     'click',
-    () => {
-      hamWrapper.click();
+    (e) => {
+      toggleHamburgerItem(e);
+      const preload = document.querySelector('.preload-wrapper');
+      if (preload) preload.style.display = 'none';
     },
     false,
   );
 }
-
-// function toggleInstanceDirection() {
-//   var direction = instance.direction;
-//   if (direction !== 'alternate') {
-//     instance.direction = direction !== 'normal' ? 'normal' : 'reverse';
-
-//     if (instance.progress === 100 && instance.direction === 'reverse') {
-//       instance.completed = false;
-//     }
-//   }
-//   ...
-// }
